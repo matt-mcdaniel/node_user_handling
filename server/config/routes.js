bdea37b4ac6521510f81d6b2c024f4ad058fc4cd
Matt McDaniel <matt.h.mcdaniel@gmail.com>
2015-12-02 22:16:48 -0800

    Add React front-end

---
+++ server/config/routes.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var Q = require('q');
+
+module.exports = function(app) {
+
+	var users = [];
+
+	function readDir() {
+		var deferred = Q.defer();
+		fs.readdir(path.join(__dirname, '/../users'), function(err, data) {
+			if (err) console.log(err);
+			deferred.resolve(data);
+		})
+		return deferred.promise;
+	}
+
+	function getContents(filenames) {
+		return Q.all(
+			filenames.map(function(filename) {
+				return Q.nfcall(fs.readFile, path.join(__dirname, '/../users/') + filename, 'utf8').then(JSON.parse);
+			})
+		);
+	}
+
+	function getById(id, cb) {
+		users.forEach(function(v) {
+			if (v._id === id) {
+				return cb(v);
+			}
+		});
+	}
+
+	readDir()
+		.then(getContents)
+		.then(function(contents) {
+			users = contents;
+		});
+
+	app.get('/', function(req, res) {
+		res.render('users', { users: users })
+	});
+
+	app.get('/:id', function(req, res) {
+		var user = getById(req.params.id, function(user) {
+			res.render('detail', { user: user });
+		});
+	});
+
+}
+
+
+
--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
+
 var React = require('react');
-var Router = require('react-router');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+
 var engines = require('consolidate');
 var path = require('path');
 
@@ -8,16 +12,17 @@ var app = express();
 var port = 3000;
 
 app.engine('hbs', engines.handlebars);
+app.set('config', __dirname + '/config');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 
 app.get('/*', function(req, res) {
-	console.log('hey');
-}))
+	res.sendFile(path.join(__dirname + '/../app/index.html'));
+})
 
 app.listen(port, function() {
 	console.log('App listening on port', port);
 });
 
 // Bootstrap Routes
-require('./config/routes')(app);
+//require('./config/routes')(app);
